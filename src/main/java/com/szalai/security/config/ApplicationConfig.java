3f9d29f81542c3b28597ee044b485b6d08413ceb
Gergo Szalai <szerg97@gmail.com>
2023-03-17 14:52:58 +0100

    created app config

---
+++ src/main/java/com/szalai/security/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.szalai.security.config;
+
+import com.szalai.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findByEmail(username)
+                .orElseThrow();
+    }
+}
